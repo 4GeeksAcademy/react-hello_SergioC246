ffa7548520f5f4b36087a8aad244fb4be81efef8
Sergio Cordoba <sergiocordoba246@gmail.com>
2025-11-19 12:00:30 +0000

    Revisando

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="bg-dark text-white text-center py-5 mt-4">
+        <div className="bg-body-tertiary text-black text-center py-5 mt-4">
             Creado por Sergio Córdoba. © Derechos reservados. 2025
         </div>
     )
--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -2,14 +2,13 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-md bg-body-tertiary">
             <div className="container-fluid">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <a className="navbar-brand" href="#">Aprendiendo React</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <a className="navbar-brand" href="#">React</a>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">                   
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0"> 
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
